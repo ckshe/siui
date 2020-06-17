c37523de0259c9c19a2499c9e2602d2df7e47642
zengweijie <123456>
2020-06-17 09:20:44 +0800

    记录每一块板编号记录 后台管理工序管理增加工序类型字段

---
+++ tiiimmo/admin/src/main/java/com/linln/admin/produce/repository/PcbTaskPlateNoRepository.java
@@ -0,0 +1,7 @@
+package com.linln.admin.produce.repository;
+
+import com.linln.admin.produce.domain.PcbTaskPlateNo;
+import com.linln.modules.system.repository.BaseRepository;
+
+public interface PcbTaskPlateNoRepository extends BaseRepository<PcbTaskPlateNo,Long> {
+}
