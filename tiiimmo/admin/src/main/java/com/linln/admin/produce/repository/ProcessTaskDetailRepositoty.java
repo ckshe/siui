0ecdcabb7f88628a94ed3ab9887cd251b0ef22c1
zengweijie <123456>
2020-07-09 14:10:44 +0800

    fix:拆分工序任务日计划编辑接口bug修复

--- tiiimmo/admin/src/main/java/com/linln/admin/produce/repository/ProcessTaskDetailRepositoty.java
+++ tiiimmo/admin/src/main/java/com/linln/admin/produce/repository/ProcessTaskDetailRepositoty.java
@@ -2,8 +2,10 @@ package com.linln.admin.produce.repository;
 
 import com.linln.admin.produce.domain.ProcessTaskDetail;
 import com.linln.modules.system.repository.BaseRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 public interface ProcessTaskDetailRepositoty  extends BaseRepository<ProcessTaskDetail,Long> {
@@ -12,6 +14,8 @@ public interface ProcessTaskDetailRepositoty  extends BaseRepository<ProcessTask
     @Query(value = "select * from produce_process_task_detail where process_task_code = ?1",nativeQuery = true)
     List<ProcessTaskDetail> findByProcess_task_code(String processTaskCode);
 
+    @Modifying
+    @Transactional
     @Query(value = "delete from produce_process_task_detail where process_task_code = ?1",nativeQuery = true)
     void deleteByByProcess_task_code(String processTaskCode);
 
