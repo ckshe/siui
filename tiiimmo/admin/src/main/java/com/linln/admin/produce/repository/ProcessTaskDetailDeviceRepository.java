b9534f5cabbbac334430e88ca1eb13080c401e94
zengweijie <123456>
2020-07-24 14:11:10 +0800

    日计划再拆分工站计划平板端

--- tiiimmo/admin/src/main/java/com/linln/admin/produce/repository/ProcessTaskDetailDeviceRepository.java
+++ tiiimmo/admin/src/main/java/com/linln/admin/produce/repository/ProcessTaskDetailDeviceRepository.java
@@ -16,6 +16,10 @@ public interface ProcessTaskDetailDeviceRepository extends BaseRepository<Proces
     @Query(value = "select * from produce_process_task_detail_device where process_task_code = ?1 and  CONVERT ( VARCHAR ( 100 ), plan_day_time, 23 ) = ?2 and device_code = ?3",nativeQuery = true)
     List<ProcessTaskDetailDevice> findByTaskCodeAndDayTimeAndDeviceCode(String processTaskCode,String planDayTime,String deviceCode);
 
+
+    @Query(value = "select * from produce_process_task_detail_device where process_task_code = ?1 and  CONVERT ( VARCHAR ( 100 ), plan_day_time, 23 ) = ?2 and device_code = ?3",nativeQuery = true)
+    ProcessTaskDetailDevice findByTaskCodeAndDayTimeAndDeviceCode1(String processTaskCode,String planDayTime,String deviceCode);
+
     @Modifying
     @Transactional
     @Query(value = "delete from produce_process_task_detail_device where process_task_code = ?1 ",nativeQuery = true)
