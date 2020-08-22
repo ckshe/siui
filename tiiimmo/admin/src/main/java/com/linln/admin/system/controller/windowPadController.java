15d65e4f6cc0c1e0821a5c8f9bfe703f34b82a68
chuckshe <853804912@qq.com>
2020-08-22 16:48:28 +0800

    feat:增加生产登记及首检界面

--- tiiimmo/admin/src/main/java/com/linln/admin/system/controller/windowPadController.java
+++ tiiimmo/admin/src/main/java/com/linln/admin/system/controller/windowPadController.java
@@ -29,38 +29,42 @@ public class windowPadController {
     }
 
     @GetMapping("/badAdd")
-    public String badAdd(){
-        return "/windowPad/badAdd";
-    }
+    public String badAdd(){ return "/windowPad/badAdd";}
 
     @GetMapping("/scancalc")
     public String scancalc(){return "/windowPad/scancalc/scancalc";}
-//系统设置
+
+    //  系统设置
     @GetMapping("/systemSet")
     public String systemSet(){
         return "/windowPad/systemSet/systemSet";
     }
-//    设备参数
+
+    //    设备参数
     @GetMapping("/circulation")
-    public String device(){
-        return "/windowPad/circulation";
-    }
-//物料明细
+    public String device(){ return "/windowPad/circulation";}
+
+    //    设备参数
+    @GetMapping("/firstQuality")
+    public String firstQuality(){ return "/windowPad/firstQuality";}
+
+    //  物料明细
     @GetMapping("/materiel")
-    public String materiel(){
-        return "/windowPad/materiel";
-    }
-// 显示PDF
+    public String materiel(){ return "/windowPad/materiel"; }
+
+    // 显示PDF
     @GetMapping("/showPDF")
     public String showPDF(){
         return "/windowPad/showPDF";
     }
-//    历史记录
+
+    //    历史记录
     @GetMapping("/history")
     public String history(){
         return "/windowPad/history";
     }
-//    设备维护
+
+    //    设备维护
     @GetMapping("/maintenance")
     public String maintenance(){
         return "/windowPad/maintenance";
