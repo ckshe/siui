3f0c9b887005e87de07927569912f65817da8e76
lhb <1292062304@qq.com>
2020-09-09 16:27:47 +0800

    贴片机维护记录表————数据保存

--- tiiimmo/admin/src/main/java/com/linln/admin/produce/enumm/CurrentReportEnum.java
+++ tiiimmo/admin/src/main/java/com/linln/admin/produce/enumm/CurrentReportEnum.java
@@ -11,7 +11,8 @@ public enum CurrentReportEnum {
     HHLTB("3", "后焊流通表"),
     DLBTS("4", "电路板调试"),
     SJJLB("5", "首检记录表"),
-    YSJWHJLB("6", "G5全自动视觉印刷机维护记录表");
+    YSJWHJLB("6", "G5全自动视觉印刷机维护记录表"),
+    TPJWHJLB("7", "贴片机日常维护记录表");
 
     private String name;
 
