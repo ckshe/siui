474dc4c8e37baec1cb2e1dc8b2e6520a0f417c18
lhb <1292062304@qq.com>
2020-09-09 17:28:30 +0800

    AOI维护记录表————数据保存

--- tiiimmo/admin/src/main/java/com/linln/admin/produce/enumm/CurrentReportEnum.java
+++ tiiimmo/admin/src/main/java/com/linln/admin/produce/enumm/CurrentReportEnum.java
@@ -12,7 +12,8 @@ public enum CurrentReportEnum {
     DLBTS("4", "电路板调试"),
     SJJLB("5", "首检记录表"),
     YSJWHJLB("6", "G5全自动视觉印刷机维护记录表"),
-    TPJWHJLB("7", "贴片机日常维护记录表");
+    TPJWHJLB("7", "贴片机日常维护记录表"),
+    AOIWHJLB("8", "AOI维护记录表");
 
     private String name;
 
