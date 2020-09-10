04e85605245419398731ccca5b91e0544cb0d6da
lhb <1292062304@qq.com>
2020-09-10 15:11:11 +0800

    波峰焊接机日常维护记录表————数据保存

--- tiiimmo/admin/src/main/java/com/linln/admin/produce/enumm/CurrentReportEnum.java
+++ tiiimmo/admin/src/main/java/com/linln/admin/produce/enumm/CurrentReportEnum.java
@@ -9,10 +9,11 @@ public enum CurrentReportEnum {
     HHLTB("3", "后焊流通表"),
     DLBTS("4", "电路板调试"),
     SJJLB("5", "首检记录表"),
-    YSJWHJLB("6", "G5全自动视觉印刷机维护记录表"),
-    TPJWHJLB("7", "贴片机日常维护记录表"),
+    YSJWHJLB("6", "印刷机维护记录表"),
+    TPJWHJLB("7", "贴片机维护记录表"),
     AOIWHJLB("8", "AOI维护记录表"),
-    HJJWHJLB("9", "焊接机维护记录表");
+    HJJWHJLB("9", "焊接机维护记录表"),
+    BFHWHJLB("10", "波峰焊维护记录表");
 
 
     private String name;
